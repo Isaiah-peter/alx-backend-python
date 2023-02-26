944ebccda924f86e9b8da78585ddf62e44601669
Isaiah peter <peterisaiah4fun@gmail.com>
2023-02-26 17:35:07 +0100

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, answer):
         """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), answer)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        """method to test that the method returns error"""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+            self.assertEqual(error.exception.args[0], path[-1])
