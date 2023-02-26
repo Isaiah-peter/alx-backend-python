b2a357350d4d7f00837b6e49aa0a7419ceee6c37
Isaiah peter <peterisaiah4fun@gmail.com>
2023-02-26 20:12:23 +0100

    fix pycode

--- 0x03-Unittests_and_integration_tests/test.py
+++ 0x03-Unittests_and_integration_tests/test.py
@@ -4,8 +4,8 @@ from test_utils import TestAccessNestedMap
 
 maps = TestAccessNestedMap()
 
-one = maps.test_access_nested_map({"a": 1}, ("a",), 1)
-two = maps.test_access_nested_map({"a": {"b": 2}}, ("a",), {"b": 2})
+one = maps.test_access_nested_map({"a": 1}, ("a"), 1)
+two = maps.test_access_nested_map({"a": {"b": 2}}, ("a"), {"b": 2})
 three = maps.test_access_nested_map({"a": {"b": 2}}, ("a", "b"), 2)
 
 print(one)
